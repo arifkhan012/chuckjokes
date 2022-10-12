630036cb1d9573edffe19b5b553a1d68556fea6b
Ariful Islam <arifair@Arifuls-MacBook-Air.local>
2022-10-13 02:15:53 +0600

    footer design

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="footer"
+      style={{
+        backgroundImage: `url('/image/bitmap_2.png`,
+        backgroundPosition: "center" /* Center the image */,
+        backgroundRepeat: "no-repeat" /* Do not repeat the image */,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="container">
+        <div className="footerTitle">
+          Got Jokes? get Paid <br /> for submitting!
+        </div>
+        <Link className="summitJoke" to="#">
+          Submit Joke{" "}
+          <img src={require("../../image/path-copy-5.png")} alt="img" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
